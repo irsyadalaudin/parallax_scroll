37628993808bc659ec27b229bf8915c36b73d5b1
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-02-12 13:11:45 +0200

    change from REGULER FUNCTION to ARROW FUNCTION =>   in script.js

--- script.js
+++ script.js
@@ -6,19 +6,9 @@ let btn = document.getElementById('btn')
 let mountains_front = document.getElementById('mountains_front');
 let header = document.querySelector('header');
 
-window.addEventListener('scroll', function() {
-    let value = window.scrollY;
-    stars.style.left = value * 0.25 + 'px';
-    moon.style.top = value * 1.05 + 'px';
-    mountains_behind.style.top = value * 0.05 + 'px';
-    mountains_front.style.top = value * 0 + 'px';
-    text.style.marginRight = value * 4 + 'px';
-    text.style.marginTop = value * 1.5 + 'px';
-    btn.style.marginTop = value * 1.5 + 'px';
-    header.style.top = value * 1 + 'px';
-})
 
-// window.addEventListener('scroll', (event) => {
+/* REGULAR FUNCTION  */
+// window.addEventListener('scroll', function() {
 //     let value = window.scrollY;
 //     stars.style.left = value * 0.25 + 'px';
 //     moon.style.top = value * 1.05 + 'px';
@@ -29,3 +19,17 @@ window.addEventListener('scroll', function() {
 //     btn.style.marginTop = value * 1.5 + 'px';
 //     header.style.top = value * 1 + 'px';
 // })
+
+
+/* ARROW FUNCTION */
+window.addEventListener('scroll', (event) => {
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountains_behind.style.top = value * 0.05 + 'px';
+    mountains_front.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 4 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+    header.style.top = value * 1 + 'px';
+})
